abdc5af9f5abd6dc863aa95aa8e65be1ca25b05e
Litvinenka <letuwwa@gmail.com>
2022-09-05 10:59:04 +0400

    feat: mongo client

---
+++ db/mongo_config.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+const uri = "mongodb://user:pass@localhost:27017/?maxPoolSize=20&w=majority"
+
+func MongoClient() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected and pinged.")
+	return client
+}
