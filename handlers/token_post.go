9a5d1b9e2bf04b421f9bdf6b6fe70d0bdbb51f49
Litvinenka <letuwwa@gmail.com>
2022-11-06 12:58:55 +0400

    fix: token post and db name for mongo

---
+++ handlers/token_post.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"context"
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"net/http"
+	"passwords_api/mongo_db"
+	"passwords_api/structures"
+	"passwords_api/utils"
+)
+
+func TokenPost(c echo.Context) error {
+	requestToken := new(structures.RequestToken)
+	if err := c.Bind(requestToken); err != nil {
+		return c.JSON(http.StatusBadRequest, "bad request - token")
+	}
+
+	token, _ := utils.ValidateJWT(requestToken.TokenValue)
+	claims := token.Claims.(jwt.MapClaims)
+	userData := structures.UserData{
+		UserName:     claims["username"].(string),
+		PasswordHash: claims["password_hash"].(string),
+		DatabaseName: claims["database_name"].(string),
+	}
+
+	collection := mongo_db.MongoClient().Database(userData.DatabaseName).Collection("tokens")
+
+	_, err := collection.InsertOne(context.TODO(), bson.D{
+		{Key: "username", Value: userData.UserName},
+		{Key: "password_hash", Value: userData.PasswordHash},
+	})
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "internal server error - unable to save values")
+	}
+
+	return c.JSON(http.StatusCreated, "created - values were saved")
+}
