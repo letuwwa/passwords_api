cca76c19bd98c98f194dee7631a7279c0cd0c28f
Litvinenka <letuwwa@gmail.com>
2022-11-06 12:51:50 +0400

    fix: field rename

--- structures/user_data.go
+++ structures/user_data.go
@@ -1,7 +1,7 @@
 package structures
 
 type UserData struct {
-	UserName       string `json:"username"`
-	PasswordHash   string `json:"password_hash"`
-	CollectionName string `json:"collection_name"`
+	UserName     string `json:"username"`
+	PasswordHash string `json:"password_hash"`
+	DatabaseName string `json:"database_name"`
 }
