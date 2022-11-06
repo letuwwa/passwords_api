51c6f7f2850ef2da125749faf497725578a2169c
Litvinenka <letuwwa@gmail.com>
2022-11-06 13:42:00 +0400

    feat: token password hash check

--- handlers/token_check.go
+++ handlers/token_check.go
@@ -5,47 +5,40 @@ import (
 	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
 	"passwords_api/mongo_db"
+	"passwords_api/structures"
 	"passwords_api/utils"
 )
 
-type RequestToken struct {
-	TokenValue string `json:"token"`
-}
-
-type UserData struct {
-	UserName     string `json:"user_name"`
-	PasswordHash string `json:"password_hash"`
-}
-
-func TokenPost(c echo.Context) error {
-	coll := mongo_db.MongoClient().Database("mongo_app").Collection("tokens")
-	requestToken := new(RequestToken)
+func TokenCheck(c echo.Context) error {
+	requestToken := new(structures.RequestToken)
 	if err := c.Bind(requestToken); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
+		return c.JSON(http.StatusBadRequest, "bad request - token")
 	}
+
 	token, _ := utils.ValidateJWT(requestToken.TokenValue)
 	claims := token.Claims.(jwt.MapClaims)
-	userData := UserData{
-		UserName:     claims["user_name"].(string),
+	userData := structures.UserData{
+		UserName:     claims["username"].(string),
 		PasswordHash: claims["password_hash"].(string),
+		DatabaseName: claims["database_name"].(string),
 	}
+
+	collection := mongo_db.MongoClient().Database(userData.DatabaseName).Collection("tokens")
+
 	var result bson.M
-	err := coll.FindOne(context.TODO(), bson.D{{"user_name", userData.UserName}}).Decode(&result)
+	err := collection.FindOne(context.TODO(), bson.D{
+		{"username", userData.UserName},
+	}).Decode(&result)
+
 	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			doc := bson.D{{"user_name", userData.UserName}, {"password_hash", userData.PasswordHash}}
-			_, err := coll.InsertOne(context.TODO(), doc)
-			if err != nil {
-				return c.JSON(http.StatusBadRequest, err.Error())
-			}
-			return c.JSON(http.StatusCreated, "created")
-		}
+		return c.JSON(http.StatusBadRequest, "bad request - unable  to find")
 	}
+
 	if userData.PasswordHash != result["password_hash"] {
-		return c.JSON(http.StatusForbidden, "forbidden")
+		return c.JSON(http.StatusForbidden, "forbidden - not valid hash")
 	}
+
 	return c.JSON(http.StatusOK, "ok")
 }
