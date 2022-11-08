e0c78db84dfda3e2c49e31e639f5ba90bcc2de04
Litvinenka <letuwwa@gmail.com>
2022-11-08 12:49:51 +0400

    fix: root view and CORS

--- server.go
+++ server.go
@@ -3,16 +3,29 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 	"passwords_api/handlers"
 )
 
 func main() {
 	e := echo.New()
+
+	// Logging Middleware
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "${method} - ${uri} - ${status} - ${time_rfc3339_nano}\n",
 	}))
 
+	// CORS Middleware
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "root view\n")
+	})
 	e.POST("/token", handlers.TokenPost)
 	e.POST("/token/check", handlers.TokenCheck)
+
 	e.Logger.Fatal(e.Start("localhost:5050"))
 }
