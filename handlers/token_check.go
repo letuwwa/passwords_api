b2da3f473eb7ad1f1d1ebd99e5e7d32a790f5606
Litvinenka <letuwwa@gmail.com>
2022-11-10 13:31:21 +0400

    feat: token check logging

--- handlers/token_check.go
+++ handlers/token_check.go
@@ -5,6 +5,7 @@ import (
 	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/bson"
+	"log"
 	"net/http"
 	"passwords_api/mongo_db"
 	"passwords_api/structures"
@@ -14,11 +15,13 @@ import (
 func TokenCheck(c echo.Context) error {
 	requestToken := new(structures.RequestToken)
 	if err := c.Bind(requestToken); err != nil {
+		log.Printf("bind err: %s", err.Error())
 		return c.JSON(http.StatusBadRequest, "bad request - token")
 	}
 
 	token, err := utils.ValidateJWT(requestToken.TokenValue)
 	if err != nil {
+		log.Printf("token err: %s", err.Error())
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
 
@@ -37,11 +40,13 @@ func TokenCheck(c echo.Context) error {
 	}).Decode(&result)
 
 	if err != nil {
+		log.Printf("unable to find, username: %s", userData.Username)
 		return c.JSON(http.StatusBadRequest, "bad request - unable  to find")
 	}
 
 	if userData.Password != result["password"] {
-		return c.JSON(http.StatusForbidden, "forbidden - not valid hash")
+		log.Print("invalid password")
+		return c.JSON(http.StatusForbidden, "forbidden - invalid password")
 	}
 
 	return c.JSON(http.StatusOK, "ok")
